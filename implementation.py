c8ef30d979b7514003cb42e7742f398444951f21
Carlos Perello <cac19@ic.ac.uk>
2023-08-30 18:38:26 +0100

    implement ipla

--- implementation.py
+++ implementation.py
@@ -6,25 +6,23 @@ class IPLA:
         # X0: initial data. This is a vector of size N
         self.theta = theta0
         self.X = X0
-        self.N = X0.shape[1]
+        self.N = np.shape(X0)[1]
         self.thetas = [self.theta]
         self.Xs = [self.X]
         self.gamma = gamma
-        self.grad_u_theta, self.grad_u_X = grads_u
+        self.ave_grad_u_theta, self.grad_u_X = grads_u
         self.kwargs = kwargs
 
     def grad_u_X_fn(self, theta, x):
         return self.grad_u_X(theta, x, **self.kwargs)
     
-    def grad_u_theta_fn(self, theta, x):
-        return self.grad_u_theta(theta, x, **self.kwargs)
+    def ave_grad_u_theta_fn(self, theta, x):
+        return self.ave_grad_u_theta(theta, x, **self.kwargs)
     
     def iterate(self):
-        N = self.N
-        theta_next = self.theta + self.gamma/N * np.sum([self.grad_u_theta_fn(self.theta, self.X[:,i]) for i in range(N)]) + np.sqrt(2*self.gamma/N) * np.random.normal(size=self.theta.shape)  
-        X_next = np.zeros_like(self.X)
-        for i in range(N):
-            X_next[:,i] = self.X[:,i] + self.gamma * self.grad_u_X_fn(theta_next, self.X[:,i]) + np.sqrt(2*self.gamma) * np.random.normal(size=self.X[:,i].shape)
+        D, N = np.shape(self.X)
+        theta_next = self.theta - self.gamma * self.ave_grad_u_theta_fn(self.theta, self.X) + np.sqrt(2*self.gamma) * np.random.normal(size=1)
+        X_next = self.X - self.gamma * self.grad_u_X_fn(self.theta, self.X) + np.sqrt(2*self.gamma) * np.random.normal(size=(D, N))
         self.thetas.append(theta_next)
         self.Xs.append(X_next)
         self.theta = theta_next
