fix softmax

implementation.py
@@ -56,10 +56,9 @@ class SNIS_IPLA:
     
     def iterate(self):
         D, N = np.shape(self.X)
-        weights = [np.exp(-self.U_fn(self.theta, self.X[:,k])) for k in range(N)]
-        weights_stable = weights-max(weights)
-        SNIS_weights = weights_stable/sum(weights_stable)
-        print(weights)
+        U_fns = [self.U_fn(self.theta, self.X[:,k]) for k in range(N)] 
+        U_fns_stable = U_fns - max(U_fns)
+        SNIS_weights = np.exp(U_fns_stable)/sum(np.exp(U_fns_stable))
 
         theta_next = self.theta - self.gamma*np.sum([SNIS_weights[i] * self.grad_U_theta_fn(self.theta, self.X[:,i]) for i in range(N)]) + np.sqrt(2*self.gamma) * np.random.normal(size=1)
         X_next = self.X - self.gamma * self.grad_U_X_fn(self.theta, self.X) + np.sqrt(2*self.gamma) * np.random.normal(size=(D, N))