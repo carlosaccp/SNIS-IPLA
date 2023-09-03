91a07601768432ef13c0bf24c01bf710b3204820
Carlos Perello <cac19@ic.ac.uk>
2023-09-03 18:15:09 +0200

    add Stein

--- implementation.py
+++ implementation.py
@@ -1,4 +1,11 @@
 import numpy as np
+import scipy as sp
+
+def find_root(A, tol = 1e-12):
+    # finds square root of a positive semi-definite symmetric matrix A
+    eigenvalues, eigenvectors = np.linalg.eigh(A)
+    eigenvalues[eigenvalues < tol] = 0
+    return eigenvectors @ np.diag(np.sqrt(eigenvalues)) @ (eigenvectors).T
 
 class IPLA:
     def __init__(self, theta0, X0, grads_U, gamma=0.01, **kwargs):
@@ -73,6 +80,7 @@ class SNIS_IPLA:
         self.kwargs = kwargs
         self.U = U
         self.test=test
+        self.ESS_arr = []
 
     def grad_U_X_fn(self, theta, x):
         return self.grad_U_X(theta, x, **self.kwargs)
@@ -91,6 +99,9 @@ class SNIS_IPLA:
             SNIS_weights = np.exp(U_fns_stable)/sum(np.exp(U_fns_stable))
         else:
             SNIS_weights = [1/N] * N
+
+        ESS = 1/np.sum([w**2 for w in SNIS_weights])
+        self.ESS_arr.append(ESS)
         theta_next = self.theta - self.gamma*np.sum([SNIS_weights[i] * self.grad_U_theta_fn(self.theta, self.X[:,i]) for i in range(N)]) + np.sqrt(2*self.gamma/N) * np.random.normal(size=1)
         X_next = self.X - self.gamma * self.grad_U_X_fn(self.theta, self.X) + np.sqrt(2*self.gamma) * np.random.normal(size=(D, N))
         self.thetas.append(theta_next)
@@ -137,3 +148,63 @@ class fast_SNIS_IPLA:
         self.theta = theta_next
         self.X = X_next
         return None
+
+class SVGD_EM:
+    def __init__(self, theta0, X0, grads_U, kernel, grad_kernel_x1, gamma=0.01, noise=True, **kwargs):
+        # theta0: initial parameter 
+        # X0: initial data. This is a vector of size N
+        self.noise = noise
+        self.theta = theta0
+        self.X = X0
+        self.iter = 0
+        self.kernel = kernel
+        self.N = np.shape(X0)[1]
+        self.thetas = [self.theta]
+        self.Xs = [self.X]
+        self.grad_kernel_x1 = grad_kernel_x1
+        self.gamma = gamma
+        self.ave_grad_U_theta, self.grad_U_X = grads_U
+        self.kwargs = kwargs
+
+    def kernel_fn(self, x1, x2):
+        return self.kernel(x1, x2)
+    
+    def grad_kernel_x1_fn(self, x1, x2):
+        return self.grad_kernel_x1(x1, x2)
+    
+    def grad_U_X_fn(self, theta, x):
+        return self.grad_U_X(theta, x, **self.kwargs)
+    
+    def ave_grad_U_theta_fn(self, theta, x):
+        return self.ave_grad_U_theta(theta, x, **self.kwargs)
+    
+    def mass_matrix(self, X):
+        D, N = np.shape(X)
+        mass_matrix_components = [[self.kernel_fn(X[:,i], X[:,j])*(1/N)*np.eye(D) for i in range(N)] for j in range(N)]
+        mass_m = np.bmat(mass_matrix_components)
+        return mass_m
+    
+    def iterate(self):
+        D, N = np.shape(self.X)
+        theta_next = self.theta - self.gamma * self.ave_grad_U_theta_fn(self.theta, self.X) + np.sqrt(2*self.gamma/N) * np.random.normal(size=1)
+        update_term = np.zeros((D, N))
+        if self.noise:
+            mass_m = self.mass_matrix(self.X)
+            root_mass_matrix = find_root(2*mass_m)
+        for i in range(N):
+            col = np.zeros((D,1))
+            noise = np.zeros((D,1))
+            for j in range(N):
+                A = -self.gamma/N*self.kernel_fn(self.X[:,i], self.X[:,j])*self.grad_U_X_fn(theta_next, self.X[:,j].reshape(-1,1))
+                B = self.gamma/N*self.grad_kernel_x1_fn(self.X[:,i], self.X[:,j]).reshape(-1,1)
+                col += A + B
+                if self.noise:
+                    noise += root_mass_matrix[i,j] * np.random.normal(size=(D,1)) * np.sqrt(2*self.gamma)
+            update_term[:,i] = col.reshape(-1) + noise.reshape(-1)
+        X_next = self.X + update_term 
+        self.thetas.append(theta_next)
+        self.Xs.append(X_next)
+        self.theta = theta_next
+        self.X = X_next
+        return None
+
